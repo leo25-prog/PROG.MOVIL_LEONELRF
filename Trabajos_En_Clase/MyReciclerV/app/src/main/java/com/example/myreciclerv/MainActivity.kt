6fadbfbc2ed2da3823fb7465e0eb5c17e858ddf3
leo25-prog <leo.lr.xhido@gmail.com>
2022-09-28 20:04:21 -0500

    Avance en my recicler View

--- Trabajos_En_Clase/MyReciclerV/app/src/main/java/com/example/myreciclerv/MainActivity.kt
+++ Trabajos_En_Clase/MyReciclerV/app/src/main/java/com/example/myreciclerv/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.myreciclerv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myreciclerv.data.Flower.flowerList
 
 class MainActivity : AppCompatActivity() {
     lateinit var rvf: RecyclerView
@@ -19,5 +21,17 @@ class MainActivity : AppCompatActivity() {
             LinearLayoutManager.VERTICAL,
             false
         )
+
+        val adapter = FlowerAdapter(
+            flowerList(resources),{
+                Toast.makeText(
+                    applicationContext,
+                    "Flor presionada ${it.name}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        )
+
+        rvf.adapter = adapter
     }
 }
